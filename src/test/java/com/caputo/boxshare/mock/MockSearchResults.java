5fc5fe06686dcb35f9fe7efd0dc343dd499058da
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-06 19:07:22 +0000

    Implement MockSearchResults utility class.

---
+++ src/test/java/com/caputo/boxshare/mock/MockSearchResults.java
@@ -0,0 +1,40 @@
+package com.caputo.boxshare.mock;
+
+import com.caputo.boxshare.entity.PirateBaySearchResult;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * This utility class is used to read mock search results from the local "test.json" file. The
+ * returned search results are very likely not representative of the response of an actual search,
+ * as these are going to change nearly constantly.
+ */
+@Service
+public class MockSearchResults {
+  @Value("classpath:mock-search-results/test.json")
+  Resource resourceFile;
+
+  public List<PirateBaySearchResult> getAll() throws IOException {
+    Reader reader = new InputStreamReader(resourceFile.getInputStream(), UTF_8);
+    String in = FileCopyUtils.copyToString(reader);
+    return new ObjectMapper()
+        .reader()
+        .forType(new TypeReference<List<PirateBaySearchResult>>() {})
+        .readValue(in);
+  }
+
+  public PirateBaySearchResult getFirst() throws IOException {
+    return getAll().get(0);
+  }
+}
