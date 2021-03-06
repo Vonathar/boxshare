d86037baadca9563e07174b16e5b17f261addbaf
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-06 14:52:45 +0000

    Validate query in SearchController class.

--- src/main/java/com/caputo/boxshare/controller/SearchController.java
+++ src/main/java/com/caputo/boxshare/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.caputo.boxshare.controller;
 
+import com.caputo.boxshare.service.QueryValidator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SearchController {
 
+  final QueryValidator queryValidator;
+
+  public SearchController(QueryValidator queryValidator) {
+    this.queryValidator = queryValidator;
+  }
+
   @GetMapping("search/{query}")
   public String getContent(@PathVariable String query) {
+    queryValidator.validate(query);
     return "";
   }
 }
