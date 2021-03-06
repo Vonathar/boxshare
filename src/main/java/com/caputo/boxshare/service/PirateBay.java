041594e4c87c3d2a706f16abd3a0bbc73c04cc21
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-06 14:50:13 +0000

    Implement PirateBay class.

---
+++ src/main/java/com/caputo/boxshare/service/PirateBay.java
@@ -0,0 +1,46 @@
+package com.caputo.boxshare.service;
+
+import com.caputo.boxshare.entity.PirateBaySearchResult;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PirateBay {
+
+  /**
+   * Queries TPB and fetches the deserialised search results for a given query.
+   *
+   * @param query the term to search on TPB.
+   * @return the deserialised search results.
+   */
+  public List<PirateBaySearchResult> search(String query) {
+    String URL = "https://pirateproxy.ltda/newapi/q.php?q=" + query;
+    String json = null;
+    try {
+      HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+      HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(URL));
+      json = request.execute().parseAsString();
+    } catch (Exception e) {
+      System.out.printf("Failed to query TBP for: \"%s\". Error: %s", query, e);
+    }
+    ObjectMapper mapper = new ObjectMapper();
+    List<PirateBaySearchResult> searchResult = null;
+    try {
+      searchResult =
+          mapper
+              .reader()
+              .forType(new TypeReference<List<PirateBaySearchResult>>() {})
+              .readValue(json);
+    } catch (Exception e) {
+      System.out.printf("Failed to parse JSON for query: \"%s\". Error: %s", query, e);
+    }
+    return searchResult;
+  }
+}
