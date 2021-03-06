017cb3fbf0ad828c6876b473234774e979659d4d
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-06 19:09:22 +0000

    Add tests for getMagnet method in PirateBay class.

--- src/test/java/com/caputo/boxshare/service/PirateBayTest.java
+++ src/test/java/com/caputo/boxshare/service/PirateBayTest.java
@@ -1,22 +1,33 @@
 package com.caputo.boxshare.service;
 
 import com.caputo.boxshare.entity.PirateBaySearchResult;
+import com.caputo.boxshare.mock.MockSearchResults;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PirateBayTest {
 
   @Autowired PirateBay pb;
+  @Autowired MockSearchResults mockSearchResults;
 
   @Test
   void search_LegalQuery_ShouldReturnListOfSearchResults() {
     List<PirateBaySearchResult> searchResults = pb.search("test");
     assertThat(searchResults).isNotNull();
+  }
+
+  @Test
+  void getMagnet_LegalSearchResult_ShouldReturnMagnetUrl() throws IOException {
+    String expected = "magnet:?xt=urn:btih:8B6833C3A0980B048F2AA94F12E5CFD7970D5288";
+    String actual = pb.getMagnet(mockSearchResults.getFirst());
+    assertEquals(expected, actual);
   }
 }
