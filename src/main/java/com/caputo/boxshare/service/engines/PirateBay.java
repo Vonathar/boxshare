f71ac080425c23a790ebd4e2d487b0580270e101
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-07 13:49:16 +0000

    Refactor 'search' method in PirateBay class to return a list of SearchResult(s).

--- src/main/java/com/caputo/boxshare/service/engines/PirateBay.java
+++ src/main/java/com/caputo/boxshare/service/engines/PirateBay.java
@@ -1,6 +1,8 @@
-package com.caputo.boxshare.service;
+package com.caputo.boxshare.service.engines;
 
+import com.caputo.boxshare.builder.SearchResultBuilder;
 import com.caputo.boxshare.entity.PirateBaySearchResult;
+import com.caputo.boxshare.entity.SearchResult;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.http.GenericUrl;
@@ -9,18 +11,25 @@ import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PirateBay {
 
+  final SearchResultBuilder srBuilder;
+
+  public PirateBay(SearchResultBuilder srBuilder) {
+    this.srBuilder = srBuilder;
+  }
+
   /**
-   * Queries TPB and fetches the deserialised search results for a given query.
+   * Queries TPB and fetches the deserialised search results.
    *
    * @param query the term to search on TPB.
    * @return the deserialised search results.
    */
-  public List<PirateBaySearchResult> search(String query) {
+  public List<SearchResult> search(String query) {
     String URL = "https://pirateproxy.ltda/newapi/q.php?q=" + query;
     String json = null;
     try {
@@ -30,17 +39,26 @@ public class PirateBay {
     } catch (Exception e) {
       System.out.printf("Failed to query TBP for: \"%s\". Error: %s", query, e);
     }
-    ObjectMapper mapper = new ObjectMapper();
-    List<PirateBaySearchResult> searchResult = null;
+    List<SearchResult> results = new ArrayList<>();
     try {
-      searchResult =
-          mapper
+      List<PirateBaySearchResult> pbResults =
+          new ObjectMapper()
               .reader()
               .forType(new TypeReference<List<PirateBaySearchResult>>() {})
               .readValue(json);
+      pbResults.forEach(
+          pbResult ->
+              results.add(
+                  srBuilder
+                      .setName(pbResult.getName())
+                      .setSeeders(pbResult.getSeeders())
+                      .setInfoHash(pbResult.getInfoHash())
+                      .setUser(pbResult.getUsername())
+                      .setSize(pbResult.getSize())
+                      .build()));
     } catch (Exception e) {
       System.out.printf("Failed to parse JSON for query: \"%s\". Error: %s", query, e);
     }
-    return searchResults;
+    return results;
   }
 }
--- src/test/java/com/caputo/boxshare/service/engines/PirateBayTest.java
+++ src/test/java/com/caputo/boxshare/service/engines/PirateBayTest.java
@@ -1,16 +1,14 @@
-package com.caputo.boxshare.service;
+package com.caputo.boxshare.service.engines;
 
-import com.caputo.boxshare.entity.PirateBaySearchResult;
+import com.caputo.boxshare.entity.SearchResult;
 import com.caputo.boxshare.mock.MockSearchResults;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PirateBayTest {
@@ -20,7 +18,7 @@ class PirateBayTest {
 
   @Test
   void search_LegalQuery_ShouldReturnListOfSearchResults() {
-    List<PirateBaySearchResult> searchResults = pb.search("test");
+    List<SearchResult> searchResults = pb.search("test");
     assertThat(searchResults).isNotNull();
   }
 }
