3e77288444fea517c252d5ef0e4cdfc4ef5270ed
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-13 21:19:04 +0000

    BS-014: Allow PirateBay class to search using a given SearchMethod.

--- src/main/java/com/caputo/boxshare/service/engines/PirateBay.java
+++ src/main/java/com/caputo/boxshare/service/engines/PirateBay.java
@@ -12,15 +12,24 @@ import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PirateBay implements SearchEngine {
+
   final SearchResultBuilder srBuilder;
   Logger logger = LoggerFactory.getLogger(PirateBay.class);
+
+  @Value("${search.smart.minSeeders}")
+  private int SMART_SEARCH_MIN_SEEDS;
+
+  @Value("${search.smart.maxResults}")
+  private int SMART_SEARCH_MAX_RESULTS;
 
   public PirateBay(SearchResultBuilder srBuilder) {
     this.srBuilder = srBuilder;
@@ -33,7 +42,8 @@ public class PirateBay implements SearchEngine {
    * @return the deserialised search results.
    */
   public List<SearchResult> search(String query, SearchMethod method) {
-    logger.info("Searching for \"{}\" on TPB..", query);
+    List<SearchResult> results = new ArrayList<>();
+    logger.info("Searching for \"{}\" on TPB ({})..", query, method);
     String URL = "https://pirateproxy.ltda/newapi/q.php?q=" + query;
     String json = null;
     try {
@@ -43,28 +53,51 @@ public class PirateBay implements SearchEngine {
     } catch (Exception e) {
       logger.error("Failed to query TBP for: \"{}\". Error: {}", query, e);
     }
-    List<SearchResult> results = new ArrayList<>();
     try {
       List<PirateBaySearchResult> pbResults =
           new ObjectMapper()
               .reader()
               .forType(new TypeReference<List<PirateBaySearchResult>>() {})
               .readValue(json);
-      pbResults.forEach(
-          pbResult ->
-              results.add(
-                  srBuilder
-                      .setName(pbResult.getName())
-                      .setSeeders(pbResult.getSeeders())
-                      .setInfoHash(pbResult.getInfoHash())
-                      .setUser(pbResult.getUsername())
-                      .setSize(pbResult.getSize())
-                      .setOrigin(this.getClass().getSimpleName())
-                      .build()));
+      if (!pbResults.get(0).getName().equals("No results returned")) {
+        switch (method) {
+          case QUICK:
+            results.add(parseResult(pbResults.get(0)));
+            break;
+          case COMPLETE:
+            pbResults.forEach(r -> results.add(parseResult(r)));
+            break;
+          case SMART:
+            List<PirateBaySearchResult> filteredResults =
+                pbResults.stream()
+                    .filter(r -> r.getSeeders() > SMART_SEARCH_MIN_SEEDS)
+                    .collect(Collectors.toList());
+            for (int i = 0; i < filteredResults.size() && i < SMART_SEARCH_MAX_RESULTS; i++) {
+              results.add(parseResult(filteredResults.get(i)));
+            }
+        }
+      }
     } catch (Exception e) {
       logger.error("Failed to parse JSON for query: \"{}\". Error: {}", query, e);
     }
     logger.info("Successfully found {} search results.", results.size());
     return results;
+  }
+
+  /**
+   * Builds a SearchResult object from an instance of PirateBaySearchResult.
+   *
+   * @param sr the instance to transform into SearchResult
+   * @return the object parsed from PirateBaySearchResult.
+   */
+  private SearchResult parseResult(PirateBaySearchResult sr) {
+    return srBuilder
+        .setName(sr.getName())
+        .setSeeders(sr.getSeeders())
+        .setInfoHash(sr.getInfoHash())
+        .setUser(sr.getUsername())
+        .setSize(sr.getSize())
+        .setOrigin(this.getClass().getSimpleName())
+        .build();
   }
 }
