3383a4e7cacabcb135fc7ab964d1c959c007fc83
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-13 16:06:34 +0000

    BS-003: Add tests for TorrentSearcher class.

---
+++ src/test/java/com/caputo/boxshare/service/TorrentSearcherTest.java
@@ -0,0 +1,37 @@
+package com.caputo.boxshare.service;
+
+import com.caputo.boxshare.DirectoryReader;
+import com.caputo.boxshare.entity.ResultList;
+import com.caputo.boxshare.enumerable.SearchMethod;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class TorrentSearcherTest {
+
+  @Autowired TorrentSearcher searcher;
+  @Autowired DirectoryReader directoryReader;
+
+  @Test
+  void get_LegalQuery_ShouldReturnAggregatedSearchResultsFromAllEngines() {
+    List<String> expectedEngines = directoryReader.getEngines();
+    List<String> actualEngines = new ArrayList<>();
+    ResultList results = searcher.get("sample test", SearchMethod.SMART);
+    results
+        .getAll()
+        .forEach(
+            r -> {
+              String origin = r.getOrigin();
+              if (!actualEngines.contains(origin)) {
+                actualEngines.add(origin);
+              }
+            });
+    assertTrue(actualEngines.containsAll(expectedEngines));
+  }
+}
