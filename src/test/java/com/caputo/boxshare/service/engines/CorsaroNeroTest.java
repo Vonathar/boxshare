4ff16d615e9395aaa39105a5d2f95908a1219fd0
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-20 15:20:04 +0000

    Add tests for CorsaroNero class.

---
+++ src/test/java/com/caputo/boxshare/service/engines/CorsaroNeroTest.java
@@ -0,0 +1,59 @@
+package com.caputo.boxshare.service.engines;
+
+import com.caputo.boxshare.entity.SearchResult;
+import com.caputo.boxshare.enumerable.SearchMethod;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+class CorsaroNeroTest {
+
+  @Autowired CorsaroNero corsaroNero;
+  private List<SearchResult> quickResults;
+  private List<SearchResult> smartResults;
+  private List<SearchResult> completeResults;
+
+  @Value("${search.smart.minSeeders}")
+  private int SMART_SEARCH_MIN_SEEDS;
+
+  @Value("${search.smart.maxResults}")
+  private int SMART_SEARCH_MAX_RESULTS;
+
+  @BeforeAll
+  public void getResults() {
+    quickResults = corsaroNero.search("linux", SearchMethod.QUICK);
+    smartResults = corsaroNero.search("linux", SearchMethod.SMART);
+    completeResults = corsaroNero.search("linux", SearchMethod.COMPLETE);
+  }
+
+  @Test
+  public void search_QuickSearch_ShouldReturnListWithSingleSearchResult() {
+    assertThat(quickResults).hasSize(1);
+  }
+
+  @Test
+  public void search_SmartSearch_ShouldNotReturnMoreResultsThanMaxAllowed() {
+    assertThat(smartResults).hasSizeLessThanOrEqualTo(SMART_SEARCH_MAX_RESULTS);
+  }
+
+  @Test
+  public void search_SmartSearch_ShouldNotReturnResultsWithLessSeedersThanMinAllowed() {
+    for (SearchResult sr : smartResults) {
+      assertThat(sr.getSeeders()).isGreaterThanOrEqualTo(SMART_SEARCH_MIN_SEEDS);
+    }
+  }
+
+  @Test
+  public void search_CompleteSearch_ShouldReturnListWithAtLeastOneSearchResult() {
+    assertThat(completeResults.size()).isGreaterThanOrEqualTo(1);
+  }
+}
