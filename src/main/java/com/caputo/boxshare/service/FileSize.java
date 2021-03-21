5e937ccd3714e87772e8708114409cf68a87d35d
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-21 18:05:17 +0000

    Refactor FileSize to improve exception handling and logging.

--- src/main/java/com/caputo/boxshare/service/FileSize.java
+++ src/main/java/com/caputo/boxshare/service/FileSize.java
@@ -1,6 +1,8 @@
 package com.caputo.boxshare.service;
 
 import com.caputo.boxshare.enumerable.SizeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
@@ -8,9 +10,10 @@ import java.util.regex.Pattern;
 
 @Service
 public class FileSize {
-
   private final Pattern unitPattern = Pattern.compile("([A-Za-z])+");
   private final Pattern valuePattern = Pattern.compile("([0-9]+)?\\.?([0-9]+)?");
+  private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+  private Matcher m;
 
   /**
    * Converts a string with a value to bytes.
@@ -19,16 +22,44 @@ public class FileSize {
    * @return the value of the input converted to bytes.
    */
   public double toBytes(String input) {
-    Matcher m = unitPattern.matcher(input);
-    double factor = 0;
-    if (m.find()) {
-      factor = SizeUnit.valueOf(m.group(0).toUpperCase()).getFactor();
+    try {
+      double value = parseValue(input);
+      double factor = parseFactor(input);
+      return value * factor;
+    } catch (IllegalArgumentException e) {
+      logger.error(
+          "Failed to convert input to bytes. Input: \"" + input + "\". Returning 0 as fallback..");
+      return 0;
     }
+  }
+
+  /**
+   * Parses a factor from the size unit of a given string.
+   *
+   * @param input any parsable string with a unit measurement.
+   * @return the factor to use to transform the value into bytes.
+   */
+  private double parseFactor(String input) {
+    m = unitPattern.matcher(input);
+    if (m.find()) {
+      return SizeUnit.valueOf(m.group(0).toUpperCase()).getFactor();
+    } else {
+      throw new IllegalArgumentException("Failed to parse factor. Input: \"" + input + "\"");
+    }
+  }
+
+  /**
+   * Parses a numerical value from a given string.
+   *
+   * @param input any parsable string with a value.
+   * @return the numerical value to multiply by the factor.
+   */
+  private double parseValue(String input) {
     m = valuePattern.matcher(input);
-    double value = 0;
     if (m.find()) {
-      value = Double.parseDouble(m.group(0));
+      return Double.parseDouble(m.group(0));
+    } else {
+      throw new IllegalArgumentException("Failed to parse value. Input: \"" + input + "\"");
     }
-    return value * factor;
   }
 }
