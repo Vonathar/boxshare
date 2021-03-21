e06a1ae6cc41250f0adca8ca630c9f30fbe90912
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-21 17:28:51 +0000

    Handle ResponseStatusException in ApkController, and related tests.

--- src/main/java/com/caputo/boxshare/controller/ApkController.java
+++ src/main/java/com/caputo/boxshare/controller/ApkController.java
@@ -5,12 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * This controller is responsible for all requests that concern the client APK. The APK is expected
@@ -20,10 +25,10 @@ import java.io.*;
 @RestController
 public class ApkController {
 
-  @Value("${boxshare.client.version}")
-  String APK_VERSION;
-
   Logger logger = LoggerFactory.getLogger(ApkController.class);
+
+  @Value("${boxshare.client.version}")
+  private String APK_VERSION;
 
   /**
    * Returns the latest version number of the APK.
@@ -44,28 +49,14 @@ public class ApkController {
   @GetMapping(value = "download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
   public @ResponseBody byte[] getApk() {
     logger.info("Incoming request for APK download.");
-    File apk = null;
     String fileName = "boxshare_" + APK_VERSION.replaceAll("\\.", "_") + ".apk";
-    System.out.println(fileName);
     try {
-      apk = new ClassPathResource(fileName).getFile();
-    } catch (IOException e) {
-      logger.error("Failed to get APK from class path. Error: " + e);
+      File apk = new ClassPathResource(fileName).getFile();
+      InputStream in = new FileInputStream(apk);
+      return IOUtils.toByteArray(in);
+    } catch (IOException ex) {
+      logger.error("APK not found in the resources folder: {}", fileName);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "APK not found.");
     }
-    InputStream in = null;
-    try {
-      assert apk != null;
-      in = new FileInputStream(apk);
-    } catch (FileNotFoundException e) {
-      logger.error("APK not found. Error: " + e);
-    }
-    logger.info("Returning APK to client.");
-    byte[] bytes = null;
-    try {
-      bytes = IOUtils.toByteArray(in);
-    } catch (IOException e) {
-      logger.error("Failed to read APK as input stream. Error: " + e);
-    }
-    return bytes;
   }
 }
--- src/test/java/com/caputo/boxshare/controller/ApkControllerTest.java
+++ src/test/java/com/caputo/boxshare/controller/ApkControllerTest.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
 import java.nio.file.Files;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,12 +37,24 @@ class ApkControllerTest {
   }
 
   @Test
-  public void getApk_ShouldReturnLatestVersionOfApk() throws Exception {
+  public void getApk_ApkFound_ShouldReturnLatestVersionOfApk() throws Exception {
     String fileName = "boxshare_" + APK_VERSION.replaceAll("\\.", "_") + ".apk";
     File apk = new ClassPathResource(fileName).getFile();
     this.mockMvc
         .perform(get("/download"))
         .andExpect(status().isOk())
         .andExpect(content().bytes(Files.readAllBytes(apk.toPath())));
+  }
+
+  @Test
+  public void getApk_ApkNotFound_ShouldThrow404ResponseStatusException() {
+    ApkController apkController = new ApkController();
+    ReflectionTestUtils.setField(apkController, "APK_VERSION", "0.0.0");
+    try {
+      apkController.getApk();
+    } catch (ResponseStatusException e) {
+      assertEquals(e.getReason(), "APK not found.");
+      assertEquals(e.getStatus(), HttpStatus.NOT_FOUND);
+    }
   }
 }
