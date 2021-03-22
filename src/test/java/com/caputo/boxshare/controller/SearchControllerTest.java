75edc3b72cece4291c45035a79de06cd407a83aa
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-22 21:36:23 +0000

    Improve isolation for SearchController tests.

--- src/test/java/com/caputo/boxshare/controller/SearchControllerTest.java
+++ src/test/java/com/caputo/boxshare/controller/SearchControllerTest.java
@@ -1,31 +1,38 @@
 package com.caputo.boxshare.controller;
 
-import com.caputo.boxshare.controller.mock.MockResultList;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.caputo.boxshare.entity.ResultList;
+import com.caputo.boxshare.entity.SearchResult;
+import com.caputo.boxshare.service.QueryValidator;
+import com.caputo.boxshare.service.TorrentSearcher;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class SearchControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Mock private QueryValidator queryValidator;
+  @Mock private TorrentSearcher torrentSearcher;
 
   @Test
-  public void search_ShouldReturnSearchResultsDeserialisableByTheClient() throws Exception {
-    String result =
-        this.mockMvc
-            .perform(get("/search/sample test"))
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-    new ObjectMapper().readValue(result, MockResultList.class);
+  public void search_LegalQuery_ShouldReturn200HttpResponse() throws Exception {
+    when(queryValidator.validate(any())).thenReturn(true);
+    when(torrentSearcher.get(any(), any()))
+        .thenReturn(
+            new ResultList(Collections.singletonList(new SearchResult("n", "h", 0, "0", "o"))));
+    SearchController searchController = new SearchController(queryValidator, torrentSearcher);
+    MockMvc mockMvc = standaloneSetup(searchController).build();
+    mockMvc.perform(get("/search/test")).andExpect(status().isOk());
   }
 }
