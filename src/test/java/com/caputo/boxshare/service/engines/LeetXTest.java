5cd7a56f6b0bfbccf323691354d38bb99c25dd71
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-23 23:47:56 +0000

    Remove unnecessary calls to search engines in tests.

--- src/test/java/com/caputo/boxshare/service/engines/LeetXTest.java
+++ src/test/java/com/caputo/boxshare/service/engines/LeetXTest.java
@@ -3,60 +3,24 @@ package com.caputo.boxshare.service.engines;
 import com.caputo.boxshare.entity.SearchResult;
 import com.caputo.boxshare.enumerable.SearchMethod;
 import org.jsoup.nodes.Element;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class LeetXTest {
 
   @Autowired LeetX leetX;
-  private List<SearchResult> quickResults;
-  private List<SearchResult> smartResults;
-  private List<SearchResult> completeResults;
-
-  @Value("${search.smart.minSeeders}")
-  private int SMART_SEARCH_MIN_SEEDS;
-
-  @Value("${search.smart.maxResults}")
-  private int SMART_SEARCH_MAX_RESULTS;
-
-  @BeforeAll
-  public void getResults() {
-    quickResults = leetX.search("sample test", SearchMethod.QUICK);
-    smartResults = leetX.search("sample test", SearchMethod.SMART);
-    completeResults = leetX.search("sample test", SearchMethod.COMPLETE);
-  }
 
   @Test
-  public void search_QuickSearch_ShouldReturnListWithSingleSearchResult() {
-    assertThat(quickResults).hasSize(1);
-  }
-
-  @Test
-  public void search_SmartSearch_ShouldNotReturnMoreResultsThanMaxAllowed() {
-    assertThat(smartResults).hasSizeLessThanOrEqualTo(SMART_SEARCH_MAX_RESULTS);
-  }
-
-  @Test
-  public void search_SmartSearch_ShouldNotReturnResultsWithLessSeedersThanMinAllowed() {
-    for (SearchResult sr : smartResults) {
-      assertThat(sr.getSeeders()).isGreaterThanOrEqualTo(SMART_SEARCH_MIN_SEEDS);
-    }
-  }
-
-  @Test
-  public void search_CompleteSearch_ShouldReturnListWithAtLeastOneSearchResult() {
-    assertThat(completeResults.size()).isGreaterThanOrEqualTo(1);
+  public void search_ShouldSuccessfullyConnectAndReturnResults() {
+    List<SearchResult> results = leetX.search("1080p", SearchMethod.QUICK);
+    assertFalse(results.isEmpty());
   }
 
   @Test
