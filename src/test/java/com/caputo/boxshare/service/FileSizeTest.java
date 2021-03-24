3b14d5e3ad2b2f6a98145423312073b231cca3ad
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-24 00:04:54 +0000

    Add tests for non-parsable inputs in FileSize class.

--- src/test/java/com/caputo/boxshare/service/FileSizeTest.java
+++ src/test/java/com/caputo/boxshare/service/FileSizeTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FileSizeTest {
@@ -12,7 +13,17 @@ class FileSizeTest {
   @Autowired FileSize fs;
 
   @Test
-  public void parseBytes_ArbitraryWhitespaces_ShouldParseBytesFromStringFileSize() {
+  public void toBytes_IllegalValue_ShouldReturnZero() {
+    assertEquals(fs.toBytes("?MB"), 0);
+  }
+
+  @Test
+  public void toBytes_IllegalFactor_ShouldReturnZero() {
+    assertEquals(fs.toBytes("5.52XB"), 0);
+  }
+
+  @Test
+  public void toBytes_ArbitraryWhitespaces_ShouldParseBytesFromStringFileSize() {
     assertThat(fs.toBytes("5.87GiB")).isEqualTo(6302864506.88);
     assertThat(fs.toBytes("26.32 MiB")).isEqualTo(27598520.32);
     assertThat(fs.toBytes("146.43  KiB")).isEqualTo(149944.32);
@@ -22,7 +33,7 @@ class FileSizeTest {
   }
 
   @Test
-  public void parseBytes_ArbitraryCase_ShouldParseBytesFromStringFileSize() {
+  public void toBytes_ArbitraryCase_ShouldParseBytesFromStringFileSize() {
     assertThat(fs.toBytes("5.87 gib")).isEqualTo(6302864506.88);
     assertThat(fs.toBytes("6.36 GIB")).isEqualTo(6828998000.64);
     assertThat(fs.toBytes("1.23 gIb")).isEqualTo(1320702443.52);
@@ -32,7 +43,7 @@ class FileSizeTest {
   }
 
   @Test
-  public void parseBytes_ArbitraryDecimalPlaces_ShouldParseBytesFromStringFileSize() {
+  public void toBytes_ArbitraryDecimalPlaces_ShouldParseBytesFromStringFileSize() {
     assertThat(fs.toBytes("5.87142 gib")).isEqualTo(6304389220.27008);
     assertThat(fs.toBytes("6.36512 GIB")).isEqualTo(6834495558.77888);
     assertThat(fs.toBytes("1.23661 gIb")).isEqualTo(1327799876.97664);
