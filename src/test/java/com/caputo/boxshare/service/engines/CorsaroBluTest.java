02ab77ae740c55dccefe462b688e2458e25d434f
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-26 17:25:38 +0000

    Add tests for CorsaroBlu class.

---
+++ src/test/java/com/caputo/boxshare/service/engines/CorsaroBluTest.java
@@ -0,0 +1,30 @@
+package com.caputo.boxshare.service.engines;
+
+import com.caputo.boxshare.entity.SearchResult;
+import com.caputo.boxshare.enumerable.SearchMethod;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class CorsaroBluTest {
+
+  @Autowired CorsaroBlu corsaroNero;
+
+  @Test
+  public void search_ShouldSuccessfullyConnectAndReturnResults() {
+    List<SearchResult> results = corsaroNero.search("1080p", SearchMethod.QUICK);
+    assertFalse(results.isEmpty());
+  }
+
+  @Test
+  public void parseRow_NonParsableRow_ShouldReturnNull() {
+    assertNull(corsaroNero.parseRow(new Element("tr")));
+  }
+}
