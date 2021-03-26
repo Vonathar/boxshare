9eeb22e52fd720b715e4d903424c718af596ca01
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-26 19:04:03 +0000

    Add and initialise 'headers' map in SearchEngine interface.

--- src/main/java/com/caputo/boxshare/service/engines/SearchEngine.java
+++ src/main/java/com/caputo/boxshare/service/engines/SearchEngine.java
@@ -5,7 +5,9 @@ import com.caputo.boxshare.enumerable.SearchMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This interface represents a generic search engine, responsible for fetching torrent results from
@@ -15,6 +17,12 @@ import java.util.List;
 public interface SearchEngine {
   Logger logger = LoggerFactory.getLogger(Runnable.class.getSimpleName());
   String className = Runnable.class.getSimpleName();
+  Map<String, String> headers =
+      new HashMap<>(
+          Map.of(
+              "User-Agent",
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  + "(KHTML, like Gecko) Chrome/88.0.4324.186 Safari/537.36"));
 
   /**
    * Queries the search engine and fetches the deserialised search results.
