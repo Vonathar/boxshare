de6a8d67350a2f84bd17f9148fab769099a7f365
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-27 20:48:21 +0000

    Implement the PingController class.

---
+++ src/main/java/com/caputo/boxshare/controller/PingController.java
@@ -0,0 +1,22 @@
+package com.caputo.boxshare.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+  @Value("${server.port}")
+  private String PORT_NUMBER;
+
+  /**
+   * Returns a status message with the port number.
+   *
+   * @return the application status.
+   */
+  @GetMapping("ping")
+  public String getStatus() {
+    return "Boxshare is running on port " + PORT_NUMBER + ".";
+  }
+}
