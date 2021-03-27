d2c9da2aa109b00be73db7cb0b3eed5547868ead
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-03-27 22:51:09 +0000

    Fix typo in deploy-docs.sh

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "Vonathar/Boxshare" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk11" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "Vonathar/boxshare" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Publishing javadoc...\n"
 
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "Vonathar/Boxshare" ] && [ "$TRAVIS_JDK_VERSION" == 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Vonathar/Boxshare gh-pages >/dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Vonathar/boxshare gh-pages >/dev/null
 
   cd gh-pages
   git rm -rf ./javadoc
