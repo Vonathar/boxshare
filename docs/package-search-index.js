e025d2dfb97d326669ebdc3549317ba9db408115
travis-ci <travis@travis-ci.org>
2021-03-28 01:07:02 +0000

    Add latest documentation. Travis build #: 32

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.caputo.boxshare"},{"l":"com.caputo.boxshare.builder"},{"l":"com.caputo.boxshare.controller"},{"l":"com.caputo.boxshare.entity"},{"l":"com.caputo.boxshare.enumerable"},{"l":"com.caputo.boxshare.service"},{"l":"com.caputo.boxshare.service.engines"}]
