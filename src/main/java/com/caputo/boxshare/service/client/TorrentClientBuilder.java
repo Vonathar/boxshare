149f512a9c3f009034881fcb5c77d25b065865e6
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-04-17 16:47:18 +0100

    Implement TorrentClientBuilder class.

---
+++ src/main/java/com/caputo/boxshare/service/client/TorrentClientBuilder.java
@@ -0,0 +1,66 @@
+package com.caputo.boxshare.service.client;
+
+import bt.Bt;
+import bt.data.Storage;
+import bt.metainfo.Torrent;
+import bt.runtime.BtClient;
+import bt.runtime.Config;
+import com.caputo.boxshare.service.TorrentFileTailer;
+import java.util.function.Consumer;
+import org.springframework.stereotype.Service;
+
+/**
+ * A class responsible for building pre-configured torrent clients. All static configuration is
+ * delegated to {@link TorrentClientConfig}. For usage examples, see {@link TorrentClient}.
+ */
+@Service
+public class TorrentClientBuilder {
+
+  final Config config;
+  final Storage storage;
+  private final TorrentFileTailer torrentFileTailer;
+  private String magnetUrl;
+
+  public TorrentClientBuilder(TorrentFileTailer torrentFileTailer, Config config, Storage storage) {
+    this.torrentFileTailer = torrentFileTailer;
+    this.config = config;
+    this.storage = storage;
+  }
+
+  /**
+   * Builds and returns an instance of a BtClient, ready to be started.
+   *
+   * @return an instance of a BtClient.
+   */
+  public BtClient build() {
+    return Bt.client()
+        .afterTorrentFetched(updateTailerFileSize())
+        .config(config)
+        .magnet(magnetUrl)
+        .sequentialSelector()
+        .storage(storage)
+        .autoLoadModules()
+        .stopWhenDownloaded()
+        .build();
+  }
+
+  /**
+   * Updates the file size in the tailer using the torrent's parsed metadata.
+   *
+   * @return the callback for BtClient's afterTorrentFetched.
+   */
+  private Consumer<Torrent> updateTailerFileSize() {
+    return torrent -> torrentFileTailer.setSize((int) torrent.getSize());
+  }
+
+  /**
+   * Sets the magnet URL to be downloaded by the client.
+   *
+   * @param magnetUrl the magnet URL.
+   * @return the builder.
+   */
+  public TorrentClientBuilder setMagnetUrl(String magnetUrl) {
+    this.magnetUrl = magnetUrl;
+    return this;
+  }
+}
---
+++ src/main/java/com/caputo/boxshare/service/client/TorrentClientConfig.java
@@ -0,0 +1,50 @@
+package com.caputo.boxshare.service.client;
+
+import bt.data.Storage;
+import bt.data.file.FileSystemStorage;
+import bt.runtime.Config;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A configuration class that holds the settings used by the torrent client. This class is only
+ * responsible for the pre-build configuration. For its usage, see {@link TorrentClientBuilder}
+ */
+@Configuration
+public class TorrentClientConfig {
+
+  @Value("${torrent.client.ipaddr}")
+  private String CLIENT_IP_ADDR;
+
+  @Value("${torrent.download.directory}")
+  private String downloadDir;
+
+  @Bean
+  public Config getConfig() {
+    return new Config() {
+      @Override
+      public int getNumOfHashingThreads() {
+        return Runtime.getRuntime().availableProcessors() * 2;
+      }
+
+      @Override
+      public InetAddress getAcceptorAddress() {
+        try {
+          return InetAddress.getByName(CLIENT_IP_ADDR);
+        } catch (UnknownHostException e) {
+          e.printStackTrace();
+          return null;
+        }
+      }
+    };
+  }
+
+  @Bean
+  public Storage getStorage() {
+    return new FileSystemStorage(Paths.get(downloadDir));
+  }
+}
