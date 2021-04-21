1832f542c0438ef3b3dd3fc696d7a4bb0d35ad85
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-04-21 22:07:28 +0100

    Implement TorrentFileTailer class.

---
+++ src/main/java/com/caputo/boxshare/service/torrent/TorrentFileTailer.java
@@ -0,0 +1,79 @@
+package com.caputo.boxshare.service.torrent;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import org.springframework.stereotype.Service;
+
+/**
+ * A class that represents a file tailer, responsible for continuously keeping track of a given file
+ * for any newly added bytes. Whenever new bytes are added to the tailed file, the tailer notifies
+ * its listeners - sending them the newly-added bytes. For the interface that a listener has to
+ * implement, see {@link TorrentFileTailerListener}.
+ */
+@Service
+public class TorrentFileTailer extends Thread {
+
+  private boolean isTailing = true;
+  private File torrentFile;
+  private TorrentFileTailerListener listener;
+  private int torrentSize = 0;
+
+  public boolean isTailing() {
+    return isTailing;
+  }
+
+  public int getTorrentSize() {
+    return torrentSize;
+  }
+
+  public void setTorrentSize(int size) {
+    this.torrentSize = size;
+  }
+
+  public void setTorrentFile(File torrentFile) {
+    this.torrentFile = torrentFile;
+  }
+
+  public void setListener(TorrentFileTailerListener listener) {
+    this.listener = listener;
+  }
+
+  /**
+   * Starts the tailer to continuously monitor the file for any new bytes, which are then sent to
+   * the registered listener. The tailer automatically stops whenever the tailed file reaches the
+   * size declared in the torrent's metadata.
+   */
+  public void run() {
+    try {
+      while (!torrentFile.exists()) {
+        sleep(1000);
+      }
+      isTailing = true;
+      RandomAccessFile raf = new RandomAccessFile(torrentFile, "r");
+      long rafPointer = 0;
+      while (isTailing) {
+        long localFileLength = torrentFile.length();
+        while (localFileLength > rafPointer) {
+          byte[] buffer;
+          int PREFERRED_BUFFER_SIZE = 1024 * 1024;
+          if (localFileLength - rafPointer < PREFERRED_BUFFER_SIZE) {
+            buffer = new byte[(int) (localFileLength - rafPointer)];
+          } else {
+            buffer = new byte[1024 * 1024];
+          }
+          raf.seek(rafPointer);
+          raf.read(buffer);
+          listener.newBytes(buffer);
+          rafPointer = raf.getFilePointer();
+        }
+        if (rafPointer >= torrentSize) {
+          isTailing = false;
+        }
+        sleep(1000);
+      }
+      raf.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
